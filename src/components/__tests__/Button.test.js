f43f727b6de37a1f990cb68b15d0f577439e61bc
Anuj <anujsinha789@gmail.com>
2022-01-13 14:27:19 +0530

    Added testcase : 'should render text passed as prop' for btn component

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -2,8 +2,19 @@ import { render, screen, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Button from "../Button";
 
+afterEach(() => {
+  cleanup(); // runs after every test case and cleans up the render.
+});
+
 test("should render button component", () => {
   render(<Button />);
   const Btn = screen.getByTestId("counter_btn");
   expect(Btn).toBeInTheDocument();
+});
+
+test("should render text passed as prop", () => {
+  render(<Button text="Click me!!" />);
+  const Btn = screen.getByTestId("counter_btn");
+  expect(Btn).toHaveTextContent("Click me!!");
+  expect(Btn).not.toHaveClass(); // Button component should not have any class.
 });
